70ce727819ad8d7eb6ca786a0526b3c90871112e
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-25 16:37:42 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+	validates :name, presence: true, uniqueness: true, length: {minimum: 3}
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
+
 end
